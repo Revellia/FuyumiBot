64636e7d6a6239723cd1cd0c0f6b1137e50e134f
Revellia <hunterbaker66@gmail.com>
2021-08-02 00:47:25 -0400

    Cleaned up code

--- FuyumiBot/Commands/FunCommands.cs
+++ FuyumiBot/Commands/FunCommands.cs
@@ -10,20 +10,40 @@ namespace FuyumiBot.Commands
 {
     public class FunCommands : BaseCommandModule
     {
+
         [Command("Ping")]
+        [Description("Checks lag")]
         public async Task Ping(CommandContext ctx)
         {
-            await ctx.Channel.SendMessageAsync("Pong").ConfigureAwait(false);
+            await ctx.Channel.SendMessageAsync("Pong")
+                .ConfigureAwait(false);
         }
+
         [Command("Hello")]
+        [Description("Says hello to Fuyumi!")]
         public async Task Hello(CommandContext ctx)
         {
-            await ctx.Channel.SendMessageAsync("Hello I am Fuyumi! I was made by Revellia to help with many things!").ConfigureAwait(false);
+            await ctx.Channel.SendMessageAsync("Hello I am Fuyumi! I was made by Revellia to help with many things!")
+                .ConfigureAwait(false);
         }
+
         [Command("Info")]
+        [Description("Gives info on Fuyumi")]
         public async Task Info(CommandContext ctx)
         {
-            await ctx.Channel.SendMessageAsync("If you want info on me find my source code and more at https://github.com/Revellia/FuyumiBot").ConfigureAwait(false);
+            await ctx.Channel.SendMessageAsync("If you want info on me find my source code and more at https://github.com/Revellia/FuyumiBot")
+                .ConfigureAwait(false);
+        }
+
+        [Command("Add")]
+        [Description("Adds two numbers together")]
+        public async Task Add(CommandContext ctx,
+            [Description("First Number")] double numberOne,
+            [Description("Second Number")] double numberTwo)
+        {
+            await ctx.Channel.SendMessageAsync((numberOne + numberTwo)
+                .ToString())
+                .ConfigureAwait(false);
         }
     }
 }
