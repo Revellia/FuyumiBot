61c881139efe00f1ed6f815e577d201d738f2bb5
Revellia <hunterbaker66@gmail.com>
2021-08-05 00:52:40 -0400

    Adds info about mention bug for warn string

--- FuyumiBot/Commands/ModerationCommands.cs
+++ FuyumiBot/Commands/ModerationCommands.cs
@@ -10,17 +10,17 @@ namespace FuyumiBot.Commands
 {
     class ModerationCommands : BaseCommandModule
     {
-        //This command is huge and complicated
+        //This command still isn't completely bug free but I'm not sure how to fix it. Users can't mention another user in warning msg
         [Command("Warn")]
         [Description("Warns the user")]
         [RequireRoles(RoleCheckMode.Any, "Fuyumi's Creator", "Owner", "Moderator", "Administrator")]
         public async Task Warn(CommandContext ctx,
-    [Description("Message to warn with | \"Example warning\"")] string WarningMessage,
+    [Description("Message to warn with (Can't use mentions in message) | \"Example warning\"")] string WarningMessage,
     [Description("User to warn | @ExampleUser")] string WarnUser)
         {
             if (WarningMessage.Contains("@")){
 
-                await ctx.Member.SendMessageAsync("Command typed incorrectly, check ?help warn")
+                await ctx.Member.SendMessageAsync("Command typed incorrectly you can't use mentions in your warning, check ?help warn for more info")
                     .ConfigureAwait(false);
             } else if (WarnUser.Contains("@"))
             {
